87c1fe95201daa63b8a87ee8e45e68133820526c
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-08-12 22:16:39 +0700

    fix deploy 2

--- src/app/(pages)/phim/[slug]/page.tsx
+++ src/app/(pages)/phim/[slug]/page.tsx
@@ -6,19 +6,19 @@ import { BreadCumb } from "./BreadCumb";
 import { ActorList } from "./ActorList";
 import { Metadata } from "next";
 
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
 export const metadata: Metadata = {
   title: "Chi tiết phim",
   description: "Mô tả trang chi tiết phim",
 };
 
-export default async function MovieDetail({
-  params,
-}: {
-  params: {
-    slug: string;
-  };
-}) {
-  const { slug } = await params;
+export default async function MovieDetail({ params }: Props) {
+  const { slug } = params;
 
   // Get movie detail
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_BE}/phim/${slug}`);
